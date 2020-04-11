392389dc303bb38b888f0f6ac3fd9a24e296d2e9
ChanzeZheng <40560646+ChanzeZheng@users.noreply.github.com>
2020-04-12 01:00:03 +0800

    226号问题，翻转二叉树，难度简单

---
+++ 226_翻转二叉树.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+#define Node struct TreeNode
+struct TreeNode* invertTree(struct TreeNode* root){
+    if(root==NULL){
+        return NULL;
+    }
+    //实现反转
+    Node* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+    invertTree(root->left);
+    invertTree(root->right);
+    return root;
+}
